8d14f4230f7cbd907bc68bdcb8e01243e2d24caf
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2022-04-09 02:56:17 -0400

    in the beginning there was darkness

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+/* Copyright 2022 Danny McClanahan */
+/* SPDX-License-Identifier: AGPL-3.0-only */
+
+//! ???
+
+/* Turn all warnings into errors! */
+/* #![deny(warnings)] */
+/* Warn for missing docs in general, and hard require crate-level docs. */
+/* #![warn(missing_docs)] */
+#![deny(rustdoc::missing_crate_level_docs)]
+/* Make all doctests fail if they produce any warnings. */
+#![doc(test(attr(deny(warnings))))]
+/* Enable all clippy lints except for many of the pedantic ones. It's a shame this needs to be
+ * copied and pasted across crates, but there doesn't appear to be a way to include inner attributes
+ * from a common source. */
+#![deny(
+  clippy::all,
+  clippy::default_trait_access,
+  clippy::expl_impl_clone_on_copy,
+  clippy::if_not_else,
+  clippy::needless_continue,
+  clippy::unseparated_literal_suffix,
+  clippy::used_underscore_binding
+)]
+/* We use inner modules in several places in this crate for ergonomics. */
+#![allow(clippy::module_inception)]
+/* It is often more clear to show that nothing is being moved. */
+#![allow(clippy::match_ref_pats)]
+/* Subjective style. */
+#![allow(
+  clippy::len_without_is_empty,
+  clippy::redundant_field_names,
+  clippy::too_many_arguments
+)]
+/* Default isn't as big a deal as people seem to think it is. */
+#![allow(clippy::new_without_default, clippy::new_ret_no_self)]
+/* Arc<Mutex> can be more clear than needing to grok Orderings: */
+#![allow(clippy::mutex_atomic)]
+
+extern "C" {
+  pub fn gsl_cdf_chisq_P(x: f64, nu: f64) -> f64;
+}
+
+use rand;
+
+fn main() {
+  let factor: f64 = rand::random();
+  println!("Hello, world: {}", unsafe {
+    gsl_cdf_chisq_P(2.0, factor) * factor
+  });
+}
